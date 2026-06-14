ee5da916186e7d9a5dc9333f77c45dded7ccbd51
Luan Souza <luansilveiradesouza@outlook.com.br>
2026-06-14 18:06:58 -0300

    pagina agenda acionado

--- src/app/pages/cliente/agenda-cliente/cadastro-agenda-cliente/cadastro-agenda-cliente.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cadastro-agenda-cliente',
-  imports: [],
-  templateUrl: './cadastro-agenda-cliente.html',
-  styleUrl: './cadastro-agenda-cliente.css',
-})
-export class CadastroAgendaCliente {}
